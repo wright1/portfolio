a65998dee2c42a742db9fa7a68f8713d9cfcb8a0
wright1 <prosperwright@yahoo.co.uk>
2019-08-14 13:25:19 +0100

    first iteration of ImagePanel

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import self from "./public/images/selfImage.jpg"
 import {Title} from "./src/components/title"
 import {Blurb} from "./src/components/blurb"
 import {Name} from "./src/components/name"
+import {ImagePanel} from "./src/components/styling/styles"
 
 export default class App extends React.Component{
     state ={
@@ -28,6 +29,7 @@ export default class App extends React.Component{
             <Section>
                 <Name />
                 <Blurb />
+                <ImagePanel  className="panel"/>
             </Section> 
         }
             </>
@@ -35,3 +37,5 @@ export default class App extends React.Component{
         )
     }
 }
+
+// down chevron <div>Icons made by <a href="https://www.flaticon.com/authors/amit-jakhu" title="Amit Jakhu">Amit Jakhu</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- src/components/styling/styles.js
+++ src/components/styling/styles.js
@@ -1,4 +1,5 @@
 import styled, { css, createGlobalStyle } from "styled-components"
+import panel from "../../../public/images/selfImage.jpg"
 
 export const GlobalStyle = createGlobalStyle`
     body{
@@ -12,7 +13,8 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Shadows Into Light', cursive;
     -webkit-animation-name: fadeout;
 	-webkit-animation-duration: 3s;
-	-webkit-animation-timing-function: ease-in;
+    -webkit-animation-timing-function: ease-in;
+    }
 
     }
     html{
@@ -32,11 +34,29 @@ export const Section = styled.section`
     background-attachment: ${props=>props.attachment || 'none'};
     background-position: cover;
     overflow: hidden;
+    position: relative;
     justify-content: center;
     font-family: 'Shadows Into Light Two', cursive;
+    > .panel{
+        position: absolute;
+        top: 10vh;
+        bottom: 0;
+        right: 5vw;
+    }
     @media(min-width: 668px){
         font-size: 1.2em;
     }
     
+`
+
+export const ImagePanel = styled.div`
+width: 30vw;
+height: 80vh;
+background-image: url(${panel});
+background-repeat: no-repeat;
+background-size: cover;
+background-position: cover;
+border-radius: 10px;
+box-shadow: 0 0 5px 5px grey;
 `
 
